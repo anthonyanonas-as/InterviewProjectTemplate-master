899dc7db60eb01da8570d5a97952fadd9f6417e4
anthonyanonas-as <anthony.anonas.as@gmail.com>
2026-08-11 23:11:27 +0800

    add components in app.module

--- Client/web-client/src/app/app.module.ts
+++ Client/web-client/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms'; 
+import { HttpClientModule } from '@angular/common/http'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserTrackerComponent } from './components/user-tracker/user-tracker.component';
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserTrackerComponent,
+    AdminDashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
