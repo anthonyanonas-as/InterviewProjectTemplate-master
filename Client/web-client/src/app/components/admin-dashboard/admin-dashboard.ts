bc2b2d215703900677c2f814c7cdb5ea06eb96f9
anthonyanonas-as <anthony.anonas.as@gmail.com>
2026-08-12 08:18:27 +0800

    update admin dashboards

--- Client/web-client/src/app/components/admin-dashboard/admin-dashboard.ts
+++ Client/web-client/src/app/components/admin-dashboard/admin-dashboard.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MoodService } from '../../services/mood.service';
 
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.html',
 })
 export class AdminDashboard {
+  adminLogs: any[] = [];
+
+  constructor(private moodService: MoodService) {}
+
+  ngOnInit() {
+    this.moodService.getAdminLogs().subscribe({
+      next: (data) => this.adminLogs = data,
+      error: (err) => console.error('Failed to load admin logs', err)
+    });
+  }
 
 }
